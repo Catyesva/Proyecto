c751be54578bf92504ee60427493deda8d6bfaa9
Catyesva <121903495+Catyesva@users.noreply.github.com>
2023-01-14 06:48:19 -0500

    Ingreso Mensajero

--- mensajeros.php
+++ mensajeros.php
@@ -4,7 +4,11 @@ class Mensajeros
     private $mensajeros = array();
     function __construct(){
         $mensajeros[] = array('id'=>2,'name'=>'Juan Berrio');
-        $mensajeros[] = array('id'=>1,'name'=>'Camilo berrio');
+        $mensajeros[] = array('id'=>1,'name'=>'Camilo Berrio');
+        $mensajeros[] = array('id'=>3,'name'=>'Miguel Angel Lopez');
+        $mensajeros[] = array('id'=>4,'name'=>'Lorena Sanchez');
+        $mensajeros[] = array('id'=>5,'name'=>'Liliana Mora');
+
         $this->mensajeros = $mensajeros;
     }
 
